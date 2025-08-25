df57aeb22570cace018dff57cef936f79dc5a3b7
Tousif-Ahmad <tousifahmdkhan2002@gmail.com>
2025-08-25 23:41:03 +0500

    operators

---
+++ operators/main.js
@@ -0,0 +1,68 @@
+// ARITHMETIC, COMPARISON, LOGICAL, ASSIGNMENT, UNARY , TERNARY
+
+// 1. ARITHMETIC 
+// +, -, *, /, %, **;
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 / 2);
+// console.log(2 % 2);
+// console.log(2 ** 2);
+
+// 2. COMPARISON 
+// =, == not strict, === strict, <, <, <=, >=, != not strict, !== strict, 
+let x = 2;
+let y = x;
+
+// console.log(2 == 3);
+// console.log(2 == "2");
+// console.log(2 === "2");
+// console.log(1 < 2);
+// console.log(10 > 3);
+// console.log(2 != 2);
+// console.log(2 !== "2");
+
+// 3. ASSIGNMENT 
+// =, +=, -=, /=, %=, *=
+// let a = 12;
+// a+=3;
+// a-=11;
+// a*=2;
+// a/=2;
+// a%=3;
+// console.log(a); 
+
+// 4. LOGICAL 
+// &&, ||, !
+// AND:
+// console.log(true && true);
+// console.log(true && false);
+// OR
+// console.log(true || true);
+// console.log(true || false);
+// console.log(false || true);
+// NOT
+// console.log(!10);
+// console.log(!!8);
+// console.log(!false);
+
+// 5. UNARY 
+// + , -, !, typeof, ++, --
+// console.log(+"4"); /* it converts to a number  */
+// console.log(-"8");  
+// console.log(typeof 12);
+// let a = 12;
+// a++;
+// console.log(a);
+
+// let a = 8;
+// a++ + a;
+// console.log(a);
+
+// 6. TERNARY OPERATOR 
+// condition ? true : false 
+// 12 > 13 ? console.log("true") : console.log("False");
+
+// instance of 
+// let arr = [1,2,3,4];
+// console.log(typeof arr); /* it gives object  */
+// console.log(arr instanceof Array);
