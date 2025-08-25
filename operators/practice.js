f66954070cbbdf6e121e00ed4f5fa34047628b93
Tousif-Ahmad <tousifahmdkhan2002@gmail.com>
2025-08-26 00:03:41 +0500

    operators practice

---
+++ operators/practice.js
@@ -0,0 +1,60 @@
+// q1. 
+// let x = 10;
+// let y = 20;
+
+// if (x > 5 && y < 25){
+//     console.log("A");
+// }
+// else{
+//     console.log("B");
+// }
+// q2. 
+// let isAdmin = true;
+// let isLoggedIn = false;
+
+// if (isAdmin || isLoggedIn){
+//     console.log("Access granted");
+// }
+// else{
+//     console.log("Access denied");
+// }
+
+// q3. 
+// let temp = 35;
+// if (!(temp < 30)){
+//     console.log("Hot");
+// }
+// else{
+//     console.log("Pleasant");
+// }
+
+// q4. 
+// let  a = 0;
+// if (a){
+//     console.log("Truthy");
+// }
+// else{
+//     console.log("Falsy");
+// }
+
+// q5. 
+// let isLoggedIn = true;
+// let hasToken = false;
+// const access = isLoggedIn && hasToken ? "Allow" : "Deny";
+// console.log(access);
+
+// q6. 
+// let a = 5;
+// a++;
+// console.log(a);
+
+// q7. 
+// let x = 3;
+// let y = x++;
+// console.log(x, y);
+
+// q8. 
+// let x = 8;
+// console.log(x--);
+// console.log(x);
+
