b5d950626aa7ac8e826aa07eedbb23ec271ff322
Tousif-Ahmad <tousifahmdkhan2002@gmail.com>
2025-08-27 17:36:19 +0500

    Loops

---
+++ loops/main.js
@@ -0,0 +1,49 @@
+// LOOP => to repeat a block of code again and again.
+
+// kahan se jaana he => kahan tak jaana he => kese jaana he.
+// for loop
+// 1-50
+
+// kahan se jaana he => kab tak rukna he => kaise jaana he.
+// while loop
+// hello na Aajaye
+
+// 1. FOR LOOP
+// for (let i = 1; i<=50; i++){
+//     console.log(i);
+// }
+
+// 2. WHILE LOOP
+// start
+// while(end){
+//     change;
+// }
+
+// let i = 1;
+// while(i < 32){
+//     console.log(i);
+//     i++;
+// }
+
+// 3. DO WHILE
+// let i = 1;
+// do {
+//   console.log(i);
+//   i++;
+// } while (i > 10);
+
+// 4. BREAK 
+// for (let i = 1; i<=100; i++){
+//     console.log(i);
+//     if (i === 8){
+//         break;
+//     }
+// }
+
+// 5. CONTINUE 
+// for (let i = 1; i<=20; i++){
+//     if (i === 8){
+//         continue;
+//     }
+//     console.log(i);
+// }
---
+++ loops/practice-questions.js
@@ -0,0 +1,95 @@
+// q1.
+// for (let i = 1; i<=100; i++){
+//     console.log(i);
+// }
+
+// q2.
+// let i = 10;
+// while(i >= 1){
+//     console.log(i);
+//     i--;
+// }
+
+// q3. 
+// for (let i = 1; i<=20; i++){
+//     if (i % 2 === 0){
+//         console.log(`Even Numbers ${i}`);
+//     }
+// }
+
+// q4. 
+// let i = 1;
+// while(i <= 15){
+//     // if (i % 2 !== 0){
+//     //     console.log(`Odd numbers ${i}`);
+        
+//     // }
+//     console.log(i);
+//     i+=2;
+// }
+
+// q5. 
+// let table = 5;
+// for (let i = 1; i<=10; i++){
+//     console.log(`${table} * ${i} = ${table * i}`);
+// }
+
+// q6.
+// let sum = 0;
+// for (let i = 1; i<=100; i++){
+//     sum = sum + i;
+// }
+// console.log(sum);
+
+// q7. 
+// for (let i = 1; i<=50; i++){
+//     if (i % 3 === 0){
+//         console.log(i);
+//     }
+// }
+
+// q8. 
+// let val = +prompt("Enter a number");
+// for (let i = 1; i <= val; i++){
+//     if (i % 2 === 0){
+//         console.log(`${i} is Even`);
+//     }
+//     else{
+//         console.log(`${i} is Odd`);
+//     }
+// }
+
+// q9. 
+// for (let i = 1; i<=100; i++){
+//     if (i % 3 === 0 && i % 5 === 0 ){
+//         console.log(i);
+//     }
+// }
+
+// BREAK AND CONTINUE QUESTIONS 
+// q1.
+// for (let i = 1; i<=100; i++){
+//     if (i % 7 === 0){
+//         break;
+//     }
+//     console.log(i);
+// }
+
+// // q2.
+// for (let i = 1; i<=20; i++){
+//     if (i % 3 === 0){
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+// q3. 
+// let count = 0;
+// for (let i = 1; i<=20; i++){
+//     if (i % 2 !== 0){
+//         count++;
+//         console.log(i);
+
+//     }
+//     if (count === 5) break;
+// }
