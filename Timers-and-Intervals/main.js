73ea2c518a7520150154232dd3297358096792d3
Tousif-Ahmad <tousifahmdkhan2002@gmail.com>
2025-09-01 02:32:44 +0500

    Timers and Intervals

---
+++ Timers-and-Intervals/main.js
@@ -0,0 +1,41 @@
+// setTimeout
+// let res = setTimeout(function(){
+//     console.log("heyy");
+// }, 1000)
+// clearTimeout(res)
+
+// clearTimeout
+// let reset = setInterval(() => {
+//     console.log("sam");
+// }, 2000);
+// clearInterval(reset)
+
+// COUNT DOWN TIMER 
+// let count = 10;
+// let interval = setInterval(() => {
+//     if (count >= 1){
+//         count--;
+//         console.log(count);
+//     }
+//     else{
+//         clearInterval(interval)
+//     }
+// }, 1000);
+
+
+// let progressBar = document.querySelector(".progress-bar-fill");
+// let progressText = document.querySelector(".progress-text");
+// let count = 0;
+
+// let reset = setInterval(() => {
+//     if (count <= 99){
+//         count++;
+//         progressBar.style.width = `${count}%`;
+//         progressText.textContent = `${count}%`;
+//     }
+//     else{
+//         document.querySelector("h2").textContent = "Downloaded."
+//         clearInterval(reset)
+//     }
+// }, 2000/100);
+
